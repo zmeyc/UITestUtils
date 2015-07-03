873ed24f43ced064bd6cd4db71b5ade0a24531e9
Andrey Fidrya <af@zabiyaka.com>
2015-07-04 01:47:25 +0300

    Create ExampleApp

---
+++ ExampleApp/ExampleApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ExampleApp
+//
+//  Created by Andrey Fidrya on 04/07/15.
+//  Copyright © 2015 Zabiyaka. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ExampleApp/ExampleApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ExampleApp
+//
+//  Created by Andrey Fidrya on 04/07/15.
+//  Copyright © 2015 Zabiyaka. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
