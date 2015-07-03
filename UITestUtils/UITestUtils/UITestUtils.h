5ac0cd40d8df3ac62c4471bb2f09b44dd2d5a1a6
Andrey Fidrya <af@zabiyaka.com>
2015-07-04 01:41:10 +0300

    Initial project

---
+++ UITestUtils/UITestUtils/UITestUtils.h
@@ -0,0 +1,19 @@
+//
+//  UITestUtils.h
+//  UITestUtils
+//
+//  Created by Andrey Fidrya on 04/07/15.
+//  Copyright © 2015 Zabiyaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITestUtils.
+FOUNDATION_EXPORT double UITestUtilsVersionNumber;
+
+//! Project version string for UITestUtils.
+FOUNDATION_EXPORT const unsigned char UITestUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITestUtils/PublicHeader.h>
+
+
