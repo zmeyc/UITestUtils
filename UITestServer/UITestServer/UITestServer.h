33b755357d8e7b08bda727b1a4e26d5178d8f777
Andrey Fidrya <af@zabiyaka.com>
2015-07-04 12:27:15 +0300

    Create UITestServer subproject

---
+++ UITestServer/UITestServer/UITestServer.h
@@ -0,0 +1,19 @@
+//
+//  UITestServer.h
+//  UITestServer
+//
+//  Created by Andrey Fidrya on 04/07/15.
+//  Copyright © 2015 Zabiyaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITestServer.
+FOUNDATION_EXPORT double UITestServerVersionNumber;
+
+//! Project version string for UITestServer.
+FOUNDATION_EXPORT const unsigned char UITestServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITestServer/PublicHeader.h>
+
+
